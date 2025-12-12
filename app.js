c4cb55b9556e830c99d94a66bf9c532fd3a26c84
forest45 <ppfjcvc@gmail.com>
2025-12-12 15:08:09 +0300

    f

--- app.js
+++
@@ -1,7 +0,0 @@
-import { View } from './view.js';
-import { Search } from './search.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-    const view = new View();
-    new Search(view);
-});
